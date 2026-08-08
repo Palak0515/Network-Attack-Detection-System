284889adccaea3efd8990151fb06d6caaff911c9
Palak Sharma <palaksharma0515@gmail.com>
2026-08-08 10:41:42 +0530

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -6,10 +6,10 @@ from sklearn.metrics import accuracy_score, classification_report
 import joblib
 import os
 
-# ----------------- Load Dataset -----------------
+#load dataset
 df = pd.read_csv("kddcup.data_10_percent.csv.gz", compression='gzip', header=None)
 
-# ----------------- Column Names (important) -----------------
+#col names
 columns = [
     'duration','protocol_type','service','flag','src_bytes','dst_bytes',
     'land','wrong_fragment','urgent','hot','num_failed_logins',
@@ -27,17 +27,14 @@ columns = [
 
 df.columns = columns
 
-# ----------------- Convert label to Attack/Normal -----------------
 df['label'] = df['label'].apply(lambda x: 0 if x == 'normal.' else 1)
 
-# ----------------- Encode categorical -----------------
 le = LabelEncoder()
 
 df['protocol_type'] = le.fit_transform(df['protocol_type'])
 df['service'] = le.fit_transform(df['service'])
 df['flag'] = le.fit_transform(df['flag'])
 
-# ----------------- SELECT SAME FEATURES AS YOUR PROJECT -----------------
 features = [
     'duration','src_bytes','dst_bytes',
     'count','same_srv_rate','serror_rate',
@@ -47,12 +44,12 @@ features = [
 X = df[features]
 y = df['label']
 
-# ----------------- Train Test Split -----------------
+# train test split
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-# ----------------- Model -----------------
+# model
 model = RandomForestClassifier(
     n_estimators=100,
     max_depth=10,
@@ -62,13 +59,11 @@ model = RandomForestClassifier(
 
 model.fit(X_train, y_train)
 
-# ----------------- Evaluation -----------------
 y_pred = model.predict(X_test)
 
 print("Accuracy:", accuracy_score(y_test, y_pred))
 print("\nReport:\n", classification_report(y_test, y_pred))
 
-# ----------------- Save -----------------
 os.makedirs("model", exist_ok=True)
 joblib.dump(model, "model/model.pkl")
 
